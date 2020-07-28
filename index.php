61cc8fec0c641b250965ec7098dcba815369f7e6
egodasa <egodasa@gmail.com>
2020-07-28 09:44:08 +0700

    Perbaikan index

--- index.php
+++ index.php
@@ -3,18 +3,32 @@
 	error_reporting(E_ALL);
 	ini_set('display_errors', 1);
 	require "config/pengaturan.php";
-	require "models/database.php";
 	require "models/models.php";
+	require "models/User.php";
+	require "models/Penyakit.php";
+	require "models/Gejala.php";
+	require "models/GejalaPenyakit.php";
+	require "models/Kasus.php";
+	require "models/DetailKasus.php";
+
+	$_USER = new User($DB);
+	$_PENYAKIT = new Penyakit($DB);
+	$_GEJALA = new Gejala($DB);
+	$_GEJALA_PENYAKIT = new GejalaPenyakit($DB);
+	$_KASUS = new Kasus($DB);
+	$_DETAIL_KASUS = new DetailKasus($DB);
+
 	ob_start();
 
-	$halaman = "home.php";
+	$halaman = "home";
 
 	if(!empty($_GET['halaman']))
 	{
 		$halaman = $_GET['halaman'];
 	}
+
 	
-	require "pages/".$halaman.".php";
+	require "halaman/".$halaman.".php";
 ?>
 	
 
